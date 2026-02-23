83496e344cb038f94f057f1cd6c0e6d62c37422a
ketal <k@ketal.icu>
2026-02-23 12:54:07 +0800

    feat: add CriticalNative annotation and corresponding test

--- app/src/main/java/net/kdt/pojavlaunch/CriticalNativeTest.java
+++ app/src/main/java/net/kdt/pojavlaunch/CriticalNativeTest.java
@@ -7,7 +7,10 @@
 
 package net.kdt.pojavlaunch;
 
+import dalvik.annotation.optimization.CriticalNative;
+
 public class CriticalNativeTest {
+    @CriticalNative
     public static native void testCriticalNative(int arg0, int arg1);
 
     public static void invokeTest() {
